96518917418cdfa1dbb4e8e0cf1ebecdb2febb25
Mouhamet99 <dieyemouhamet1@gmail.com>
2021-11-15 14:01:52 +0000

    Handling  active bar menu  per section on desktop

--- js/script.js
+++ js/script.js
@@ -9,6 +9,30 @@ window.onload = ()=>{
     // svg.addEventListener("hover", (e)=>{
     //     e.style.fill = "#ce0033"
     // })
+
+    // =============
+    // Gestion  active bar menu  par section (parcours / public ) 
+    //==============
+    let publicMenusItems = document.querySelectorAll('.menu-public  p')
+    let activePublicMenuItem = document.querySelector('.menu-public  p.active')
+    let parcoursMenusItems = document.querySelectorAll('.menu-parcours  p');
+    let activeParcoursMenuItem = document.querySelector('.menu-parcours  p.active')
+
+    updateActiveItem(publicMenusItems,activePublicMenuItem )
+    updateActiveItem(parcoursMenusItems,activeParcoursMenuItem )
+    function updateActiveItem(elements, activeElement){
+        elements.forEach(element=>{
+            element.addEventListener('click', function(e){
+                console.log(activeElement)
+                activeElement.classList.toggle('active')
+                element.classList.toggle('active')
+                activeElement = element
+                console.log(activeElement)
+            })
+        })
+    }
+
+
     // =============
     // 
     //==============
@@ -16,31 +40,7 @@ window.onload = ()=>{
     let menus = document.querySelectorAll(".secondary-menu")
     
     window.addEventListener('scroll', ()=>{
-        // let section1 = sections[1].getBoundingClientRect()
-        // if(section1.y <= 0 &&  section1.y + section1.height >= 500) {
-        //     if (menus[1].classList.contains("d-none")) {
-        //         menus[1].classList.toggle("d-none")
-        //     }
-            
-        //     // console.log("display")
-        // } else if (!menus[1].classList.contains("d-none")) {
-        //     menus[1].classList.toggle("d-none")
-        //     // console.log("not display")
-        // }
         
-        // let section2 = sections[0].getBoundingClientRect()
-        // if(section2.y <= 0 &&  section2.y + section2.height >= 500) {
-        //     if (menus[0].classList.contains("d-none")) {
-        //         menus[0].classList.toggle("d-none")
-        //     }
-            
-        //     // console.log("display")
-        // } else {
-        //     if (!menus[0].classList.contains("d-none")) {
-        //         menus[0].classList.toggle("d-none")
-        //     }
-        //     // console.log("not display")
-        // }   
         sections.forEach((elt,index)=>{
 
             elt = sections[index].getBoundingClientRect()
